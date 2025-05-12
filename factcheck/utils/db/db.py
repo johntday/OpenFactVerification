f01793d56ec5b5c6711d930cf4231ea7c3c0fca2
John Day <john.day@mybobs.com>
2025-05-12 12:27:15 -0500

    database

--- factcheck/utils/db/db.py
+++ factcheck/utils/db/db.py
@@ -8,11 +8,11 @@ def format_tweet(row):
         'id': row[0],
         'user_screen_name': row[1],
         'full_text': row[2],
-        'created_at': f"{row[3].month}/{row[3].day}/{row[3].year} {row[3].minute}:{row[3].second}",
+        'created_at': f"{row[3].month}/{row[3].day}/{row[3].year} {row[3].minute}:{row[3].second} CST",
     }
 
 
-def fetch():
+def fetch() -> list[dict[str, str]] | None:
     results = []
     try:
         conn = psycopg2.connect(DATABASE_URL)
@@ -28,7 +28,6 @@ def fetch():
         for row in rows:
             results.append(format_tweet(row))
 
-        print(results)
         return results
     except Exception as e:
         print(f"Error saving tweet to database: {e}")
@@ -45,54 +44,39 @@ def fetch():
         except Exception as e:
             pass
 
+
+def update(id: str, response_fact: str):
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+
+        cur.execute(
+            """UPDATE twitter_tweets
+            SET response_fact = %s
+            where id = %s"""
+            , (
+                response_fact,
+                id
+            ))
+    except Exception as e:
+        print(f"Error saving tweet to database: {e}")
+    finally:
+        try:
+            if cur:
+                cur.close()
+        except Exception as e:
+            pass
+        try:
+            if conn:
+                conn.close()
+        except Exception as e:
+            pass
+
+
+
+
+
+
 if __name__ == "__main__":
     fetch()
 
-# def save_to_database(tweets):
-#     try:
-#         # Write tweets to postgres database
-#         conn = psycopg2.connect(DATABASE_URL)
-#         cur = conn.cursor()
-#
-#         if os.getenv('EXCLUDE_RT') == '1':
-#             tweets_to_store = [format_tweet(tweet) for tweet in tweets if not tweet.full_text.startswith('RT')]
-#         else:
-#             tweets_to_store = [format_tweet(tweet) for tweet in tweets]
-#
-#         for tweet in tweets_to_store:
-#             if tweet['full_text'].endswith("…") or len(clean_text(tweet['full_text']).strip()) < 10:
-#                 continue
-#
-#             answer = openai_utils.call_openai(
-#                 prompt=tweet['full_text'],
-#             )
-#             if not answer or not answer.answer or answer.answer == False:
-#                 continue
-#
-#             cur.execute("""
-#                 INSERT INTO twitter_tweets (
-#                     """ + TABLE_COLUMNS_STR + """
-#                 ) VALUES (%s, %s, %s, %s, %s)
-#                 ON CONFLICT (id) DO NOTHING
-#             """, (
-#                 tweet['id'],
-#                 tweet['created_at_unix'],
-#                 clean_text(tweet['full_text']),
-#                 tweet['user_screen_name'],
-#                 tweet['tweet_url']
-#                 # tweet['created_at']
-#             ))
-#         conn.commit()
-#     except Exception as e:
-#         print(f"Error saving tweet to database: {e}")
-#     finally:
-#         try:
-#             if cur:
-#                 cur.close()
-#         except Exception as e:
-#             pass
-#         try:
-#             if conn:
-#                 conn.close()
-#         except Exception as e:
-#             pass
